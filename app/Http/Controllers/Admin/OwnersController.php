9136755f0cb3db456b465b0f7c9d93f09ea8a04c
ts1226 <t.sekine2008@gmail.com>
2021-11-27 21:51:21 +0900

    pagenation

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -33,7 +33,9 @@ class OwnersController extends Controller
         //dd($e_all, $q_get, $q_first, $c_test);
 
         //var_dump($q_get);
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')
+        ->paginate(3);
+
         return view('admin.owners.index', 
         compact('owners'));
 
