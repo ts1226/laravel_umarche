19b640dfa1be1d8fcadd1b47a6372550861a11bb
ts1226 <t.sekine2008@gmail.com>
2022-05-12 11:26:20 +0900

    product初期設定

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Owner;
+use App\Models\Product;
 
 class Shop extends Model
 {
@@ -21,6 +22,11 @@ class Shop extends Model
     public function Owner()
     {
         return $this->belongsTo(Owner::class);
+    }
+
+    public function Product()
+    {
+        return $this->hasMany(Product::class);
     }
 
 }
