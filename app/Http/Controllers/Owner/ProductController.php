cf68544f5e87fcb0911f82e5dbefe1779e7c9e1e
ts1226 <t.sekine2008@gmail.com>
2022-05-31 10:53:08 +0900

    constant

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -200,11 +200,11 @@ class ProductController extends Controller
                     $product->image4 = $request->image4;
                     $product->is_selling = $request->is_selling;
                     $product->save();
-                    
-                    if($request->type === '1'){
+
+                    if($request->type === \Constant::PRODUCT_LIST['add']){
                         $newQuantity = $request->quantity;
                     }
-                    if($request->type === '2'){
+                    if($request->type === \Constant::PRODUCT_LIST['reduce']){
                         $newQuantity = $request->quantity * -1;
                     }
                     Stock::create([
